19314e47245e5bb62708ef1e2d18cf8c3ced1e3b
Lex van Os <vanoslex7@gmail.com>
2024-01-06 09:41:39 +0100

    Fixed and finished script

--- client/monitor.sh
+++ client/monitor.sh
@@ -2,19 +2,19 @@
 
 echo "Hello World!"
 
-# while true
-# do
-#     # Timestamp: date
-#     # Number of running processes: ps
-#     # Cpu usage: mpstat
-#     # Free memory: free
+while true
+do
+    # Timestamp: date
+    # Number of running processes: ps
+    # Cpu usage: mpstat
+    # Free memory: free
 
-#     timeStamp=$(date +"%H:%M:%S")
-#     processes=$(ps --no-headers | wc -l)
-#     cpuUsage=$(mpstat 1 1 | awk '/Average:/ {print 100 - $NF}')
-#     memoryUsage=$(free | awk '/Mem:/ {print $4}')
+    timeStamp=$(date +"%H:%M:%S")
+    processes=$(ps --no-headers | wc -l)
+    cpuUsage=$(mpstat 1 1 | awk '/Average:/ {print 100 - $NF}')
+    memoryUsage=$(free | awk '/Mem:/ {print $4}')
 
-#     echo "Timestamp: $timeStamp, Processes: $processes, CPU Usage: $cpuUsage%, Memory Usage: $memoryUsage"
+    echo "Timestamp: $timeStamp, Processes: $processes, CPU Usage: $cpuUsage%, Memory Usage: $memoryUsage"
     
-#     sleep 1
-# done
+    sleep 1
+done
